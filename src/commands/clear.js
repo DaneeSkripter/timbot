293ab8a89419ccd08be5e5390e4e0b053f1969e9
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-12-06 15:20:42 +0100

    base client + two commands

---
+++ src/commands/clear.js
@@ -0,0 +1,25 @@
+const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
+const Discord = require("discord.js")
+// Create a new command with the name 'hello'
+new Command({
+	name: 'sezratzpravy',
+	description: 'Tim sežere tolik zpráv, kolik chceš (ale ty po 14 dnech už mu nechutnají)',
+	// GCommands Next offers different types of commands, we will only use slash and message commands here.
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	// The function thats executed when the user uses the command.
+    arguments: [
+        new Argument({
+            name: "pocetzprav",
+            description: "Počet zpráv, kolik chceš aby Tim sežral.",
+            type: ArgumentType.INTEGER,
+            required: true
+        })
+    ],
+	run: (ctx) => {
+        ctx.channel.bulkDelete(ctx.arguments.getInteger("pocetzprav"))
+        const embed = new Discord.EmbedBuilder()
+        .setColor("Random")
+        .setTitle(`Sežráno ${ctx.arguments.getInteger("pocetzprav")} zpráv!`)
+        ctx.reply({ embeds: [embed]})
+	}
+});
---
+++ src/commands/klacek.js
@@ -0,0 +1,20 @@
+const { Command, CommandType } = require('gcommands');
+const Discord = require("discord.js")
+// Create a new command with the name 'hello'
+new Command({
+	name: 'prinesklacek',
+	description: 'Tim ti možná přinese klacek',
+	// GCommands Next offers different types of commands, we will only use slash and message commands here.
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	// The function thats executed when the user uses the command.
+	run: (ctx) => {
+		const responses = [ "Tady máš!", "Ne! Ten je teď můj..", "Chyť si mě!"]
+        const num = Math.floor(Math.random() * 3) + 1
+        console.log(num)
+        const embed = new Discord.EmbedBuilder()
+        .setColor("Random")
+        .setTitle(responses[num])
+        .setDescription("*Tady bude obrazek tima, na zaklade odpovedi*")
+        ctx.reply({ embeds: [embed]})
+	}
+});
